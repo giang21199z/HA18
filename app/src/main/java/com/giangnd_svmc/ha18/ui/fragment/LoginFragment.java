a8ffdab2ea46fdb68e29024fcf7fc7167dcb01a6
Duc Manh Nguyen <ManhND.PTIT@gmail.com>
2016-04-09 15:17:33 +0700

    fragmentLogin

--- app/src/main/java/com/giangnd_svmc/ha18/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/giangnd_svmc/ha18/ui/fragment/LoginFragment.java
@@ -1,30 +1,96 @@
 package com.giangnd_svmc.ha18.ui.fragment;
 
-import com.giangnd_svmc.ha18.app.BaseActivity;
-import com.giangnd_svmc.ha18.app.BaseFragment;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.giangnd_svmc.ha18.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by Manh on 4/9/16.
  */
-public class LoginFragment extends BaseActivity {
+public class LoginFragment extends AppCompatActivity {
+    EditText editUsername;
+    EditText editPassword;
+    Button buttonLogin;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected int getLayoutResIdContentView() {
-        return 0;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+        editUsername = (EditText) findViewById(R.id.editUsername);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onClickLogin();
+            }
+        });
+    }
+
+    private void onClickLogin(){
+        String username = editUsername.getText().toString();
+        String passwod = editPassword.getText().toString();
+        if(true){
+
+        }
+        else{
+
+        }
     }
 
     @Override
-    protected void onCreateContentView() {
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LoginFragment Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.giangnd_svmc.ha18.ui.fragment/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
-    protected int getFragmentContainerId() {
-        return 0;
-    }
+    public void onStop() {
+        super.onStop();
 
-    @Override
-    protected BaseFragment getFragmentContent() {
-        return null;
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LoginFragment Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.giangnd_svmc.ha18.ui.fragment/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
