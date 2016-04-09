e82afd4a3a9925a7f3e0a1aa8f2257591dda0935
Duc Manh Nguyen <ManhND.PTIT@gmail.com>
2016-04-09 14:42:34 +0700

    Login

---
+++ app/src/main/java/com/giangnd_svmc/ha18/ui/fragment/LoginFragment.java
@@ -0,0 +1,30 @@
+package com.giangnd_svmc.ha18.ui.fragment;
+
+import com.giangnd_svmc.ha18.app.BaseActivity;
+import com.giangnd_svmc.ha18.app.BaseFragment;
+
+/**
+ * Created by Manh on 4/9/16.
+ */
+public class LoginFragment extends BaseActivity {
+
+    @Override
+    protected int getLayoutResIdContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreateContentView() {
+
+    }
+
+    @Override
+    protected int getFragmentContainerId() {
+        return 0;
+    }
+
+    @Override
+    protected BaseFragment getFragmentContent() {
+        return null;
+    }
+}
